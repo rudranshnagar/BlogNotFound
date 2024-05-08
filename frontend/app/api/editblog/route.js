9a8377842213ece451d59106b2993e5cdc00ad74
rudranshnagar <rnagar1@stvens.edu>
2024-05-08 03:07:28 -0400

    frontend: all files

--- frontend/app/api/editblog/route.js
+++ frontend/app/api/editblog/route.js
@@ -7,8 +7,8 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "AKIAVHICLEGMPT5NJWXS",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWSACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
   },
 });
 
